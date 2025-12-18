758ca32b9def88dcc347f0f4a2804dc3bebad1cd
Ahmed Ishiba <ahmedishiba9@gmail.com>
2025-12-18 23:32:24 +0200

    Add files via upload

--- source/bank_sys.c
+++ source/bank_sys.c
@@ -1,1 +1,13 @@
+#include "bank_sys.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void login() {
+    char username[50];
+    char password[50];
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+}
