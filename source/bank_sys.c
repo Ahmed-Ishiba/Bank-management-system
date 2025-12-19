17fb5f95953ea2f6855e79e9b70ebcd3fa93ef84
Ahmed-ishiba <ahmedishiba9@gmail.com>
2025-12-19 23:50:56 +0200

    added load function and struct definition in header file

--- source/bank_sys.c
+++ source/bank_sys.c
@@ -8,4 +8,44 @@ void bank_system_init(void) {
     printf("Bank system initialized.\n");
 }
 
+void Load(){
+       FILE *fp = fopen("accounts.txt", "r");
+    if (fp == NULL) {
+        printf("Failed to open file");
+        return -1;
+    }
+    char line[300];
+    account acc;
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        char *token = strtok(line, ",");
+        strcpy(acc.accountNumber, token);
+        token = strtok(NULL, ",");
+        strcpy(acc.name, token);
+        token = strtok(NULL, ",");
+        strcpy(acc.email, token);
+        token = strtok(NULL, ",");
+        sscanf(token, "%f", &acc.balance);
+        token = strtok(NULL, ",");
+        strcpy(acc.mobile, token);
+        token = strtok(NULL, ",");
+        strcpy(acc.dateOpened, token);
+        token = strtok(NULL, ",\n");
+        strcpy(acc.status, token);
+        printf("%s, %s, %s, %.2f, %s, %d, %.2f\n",  acc.accountNumber, acc.name, acc.email,
+            acc.balance, acc.mobile, acc.dateOpened.year, acc.status);
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int check_exist(double acc_num) {
+    // Check if account exists
+    return 0; // Placeholder return value
+}
+
+int check_status(double acc_num) {
+    // Check account status
+    return 0; // Placeholder return value
+}
 
--- source/bank_sys.h
+++ source/bank_sys.h
@@ -1,28 +1,65 @@
 #ifndef BANK_SYS_H
 #define BANK_SYS_H
 
+
+typedef struct{
+    int day;
+    int month;
+    int year;
+}date;
+
+typedef struct {
+    char accountNumber[10];   
+    char name[50];
+    char email[50];
+    char mobile[11];
+    double balance;
+    date dateOpened;
+    int status;
+    double dailyWithdraw;
+} account; 
+
 extern int check_exist(double acc_num);
 extern int check_status(double acc_num);
 
 
 
 extern void bank_system_init(void);
+
 extern void login(void);
+
+extern void Load(void);
+
 extern void query_search(double acc_num);
+
 extern void advanced_search(char name[]);
+
 extern void add(void);
+
 extern void delete(double acc_num);
+
 extern void modify(double acc_num);
+
 extern void change_status(double acc_num);
+
 extern void check_limit(double acc_num);
+
 extern void withdraw(double acc_num, double amount);
+
 extern void deposit(double acc_num, double amount);
+
 extern void transfer(double from_acc, double to_acc, double amount);
+
 extern void report(int acc_num);
+
 extern void print(int acc_num);
+
 extern void save(void);
+
 extern void quit(void);
+
 extern void menu(void);
+
 extern void delete_multi(void);
 
 
